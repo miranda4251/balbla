8b7cfa9652e5bc6894b5381fee90b5c380a5c6fa
BrunoM <BR40044529@wipro.com>
2019-11-22 09:23:15 -0300

    Primeiro commit treinamento

---
+++ Bruno_agoraVAI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bruno_agoravai;
+
+/**
+ *
+ * @author BR40044529
+ */
+public class Bruno_agoraVAI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Eai_Mundo eai = new Eai_Mundo();
+        eai.Eai_Mundo();
+    }
+    
+}
---
+++ Eai_Mundo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bruno_agoravai;
+
+/**
+ *
+ * @author BR40044529
+ */
+public class Eai_Mundo {
+    public void Eai_Mundo (){
+        System.out.println("Agora vai!");
+    }
+}
